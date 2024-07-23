9662302314c63ce1bfd62c166b14884d58418885
uKaxg <uka.pgr@gmail.com>
2024-07-23 14:30:49 +0700

    chore: Add CORS middleware for handling cross-origin requests

--- index.mjs
+++ index.mjs
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import githubRouter from "./routes/github.route.mjs";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use("/api", githubRouter);
 
 app.get("/", async (req, res) => {
